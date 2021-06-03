28db96f067290c7d39c761f0c23ce016b4561653
Igor Nazarenko <igor.n.nazarenko@gmail.com>
2021-06-03 16:35:19 -0700

    More reactor reports.

--- reactor.py
+++ reactor.py
@@ -402,21 +402,22 @@ def shopper():
     ipc = market.ItemPriceCache(serv.store_db, serv.api)
 
     # name = "Sylramic Fibers[Hexite]"
-    name = "Sylramic Fibers[Hexite/Ceramic Powder]"
+    # name = "Sylramic Fibers[Hexite/Ceramic Powder]"
     # name = "Ceramic Powder"
     # name = "Sylramic Fibers[Ceramic Powder]"
     # name = "Phenolic Composites"
     # name = "Neo Mercurite"
     # name = "Phenolic Composites[Caesarium Cadmide/Silicon Diborite/Vanadium Hafnite]"
     # name = "Fullerides[Carbon Polymers]"
-    # name = "Fullerides[Platinum Technite/Carbon Polymers]"
+    name = "Fullerides[Platinum Technite/Carbon Polymers]"
+    # name = "Fullerides[Platinum Technite]"
     f = name_to_formula(w, name)
 
     print(f.output.item_type.name)
     print_price_history(ipc.get_price_history(f.output.item_type))
     print()
 
-    qty = 600
+    qty = 2000
     total = 0.0
     total_m3 = 0.0
     for i in f.inputs:
